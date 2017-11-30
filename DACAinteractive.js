53925c4e38d472de044568d0c295836bb643609b
Shanya Williams <shanya.williams24816@gmail.com>
2017-11-29 17:39:25 -0800

    upload html, css,js and images

---
+++ DACAinteractive.js
@@ -0,0 +1,24 @@
+function todayfunction(){
+	document.getElementById("button").innerHTML = "Today, the amount of people that will lose DACA is 6% of the content sent everyday on Snapchat. ";
+	document.getElementById("subtitle").innerHTML = "Today"
+}
+
+
+
+function seventeenfunction(){
+	document.getElementById("button").innerHTML = "In 2017, the amount of people at risk of losing DACA is close to the amount of minutes in a year.";
+	document.getElementById("subtitle").innerHTML = "2017"
+}
+
+
+
+function eightteenfunction(){
+	document.getElementById("button").innerHTML = "In 2018, the number of people who will lose DACA is about equal to the number of teachers employed in California.";
+	document.getElementById("subtitle").innerHTML = "2018"
+}
+
+
+function nineteenfunction(){
+	document.getElementById("button").innerHTML = "In 2019, the number of people who will have lost DACA is about the number of Apple computers sold in a month.";
+	document.getElementById("subtitle").innerHTML = "2019"
+}
